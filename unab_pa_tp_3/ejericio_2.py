ae8749fef93f2033122cbcb3ca9b4a4ac48c3e8b
Lautaa Blanco <lautaronicoblanco@hotmail.com>
2026-04-06 18:10:35 -0300

    feat: exercise 2

---
+++ unab_pa_tp_3/ejericio_2.py
@@ -0,0 +1,27 @@
+""" Crear la clase Punto con dos atributos x e y (ambos numéricos), con el correspondiente
+constructor que recibe ambos valores.
+
+● Definir métodos tales como:
+    eje_x
+    eje_y
+    impresion (método que devuelve en representación de string ambos valores)
+    opuesto (método que devuelve el punto opuesto -es decir con los atributos
+    negativos-)
+    Cualquier otro método que considere importante
+"""
+class Punto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def eje_x(self):
+        return self.x
+
+    def eje_y(self):
+        return self.y
+
+    def impresion(self):
+        return f"Punto({self.x}, {self.y})"
+
+    def opuesto(self):
+        return Punto(-self.x, -self.y)
